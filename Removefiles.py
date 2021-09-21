3098328bff2880acc621648df03cc49bd9b7712a
FlashGod123 <67604319+FlashGod123@users.noreply.github.com>
2021-09-20 17:54:06 -0700

    Add files via upload

--- Removefiles.py
+++ Removefiles.py
@@ -2,18 +2,40 @@ import time
 import os
 import shutil
 
-new = 'Time'
-path = "C:/Users/aaron/Downloads/python"
-days = time.time(path)
+
+deletedFiles = 0
+deletedFolders = 0
+path = "C:/Users/aaron/Downloads/python/test.txt"
+days = 30
+seconds = time.time()-(days*24*60*60)
+
+def getFile(path):
+    ctime = os.stat(path).st_ctime
+    return ctime
 
 if os.path.exists(path):
-    file = os.walk(path)
-else:
-    print("Path not found")
-newPath = os.path.join(path, new)
+    for root_folder,folders,files in os.walk(path):
+        if seconds>getFile(root_folder):
+            os.remove(root_folder)
+            deletedFolders += 1
 
-ctime = os.stat(path).st_ctime
-if ctime>days:
-    os.remove(path)
-else:
-    shutil.rmtree(path)
+        else:
+
+            for folder in folders:
+                folderPath = os.path.join(root_folder,folder)
+                if seconds>getFile(folderPath):
+                    os.remove(folderPath)
+                    deletedFolders += 1
+
+            for file in files:
+                filePath = os.path.join(root_folder,file)
+                if seconds>getFile(filePath):
+                    os.remove(filePath)
+                    deletedFiles += 1
+
+print("Total folders deleted:",deletedFolders)
+print("Total files deleted:",deletedFiles)
+
+
+
+
